9d95c217e26a9e118c4be2b88c805cf4287d5110
toddmurphy <toddmurphy93@gmail.com>
2020-01-17 12:57:55 -0400

    get all projects working

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -3,7 +3,18 @@ const projectDB = require('../data/helpers/projectModel');
 const router = express.Router();
 
 //get --> all projects
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  projectDB
+    .get()
+    .then(projects => {
+      res.status(200);
+      res.json(projects);
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ message: 'Sorry, no projects returned from the server', error });
+    });
+});
 
 //post --> using req.body
 router.post('/', (req, res) => {});
@@ -16,5 +27,7 @@ router.delete('/:id', (req, res) => {});
 
 //put --> updated using id and req.body
 router.put('/:id', (req, res) => {});
+
+// get --> projects by action id (save til the end)
 
 module.exports = router;
