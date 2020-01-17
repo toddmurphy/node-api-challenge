3800a5020b6add53f1ab78173a31317055a4ea4e
toddmurphy <toddmurphy93@gmail.com>
2020-01-17 12:33:04 -0400

    server set up and running and all configs working

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+// server.use('/api/projects'); //add projectsRouter
+// server.use('/api/actions'); //add actionsRouter
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Sprint challenge server and api working</h2>`);
+});
 
 module.exports = server;
