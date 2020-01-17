c425fd5d537bbc9649a12f8ffce4b35a1fec3ac9
toddmurphy <toddmurphy93@gmail.com>
2020-01-17 13:29:27 -0400

    actions get working

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,5 +1,28 @@
 const express = require('express');
-
+const actionsDB = require('../data/helpers/actionModel');
 const router = express.Router();
+
+//get --> all projects
+router.get('/', (req, res) => {
+  actionsDB
+    .get()
+    .then(actions => {
+      res.status(200);
+      res.json(actions);
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ errorMessage: 'Sorry, no actions returned from the server', error });
+    });
+});
+
+// post --> new project
+router.post('/', (req, res) => {});
+
+// delete --> delete project by id
+router.delete('/:id', (req, res) => {});
+
+// put --> update project by id and req.body
+router.put('/:id', (req, res) => {});
 
 module.exports = router;
